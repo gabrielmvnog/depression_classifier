e1886801eab3bcc2776812af06fe0696c3c0034b
C1297800 Gabriel de Melo Vaz Nogueira <c1297800@bb.com.br>
2019-09-19 08:58:39 -0300

    Insertion of pre-process in classifier

--- depression_classifier/modules/classifier.py
+++ depression_classifier/modules/classifier.py
@@ -1,5 +1,8 @@
 import pickle
 import numpy as np
+import string
+import unidecode
+import re
 
 
 class Classifier():
@@ -17,6 +20,23 @@ class Classifier():
             1: "not_depression"
         }
 
+    def __pre_process(self, sentence: str) -> str:
+
+        # Normalize to Lower
+        sentence = sentence.lower()
+
+        # Remove Accent
+        sentence = unidecode.unidecode(sentence)
+
+        # Split ponctuation
+        sentence = ' '.join(re.findall(
+            r"[\w]+|[" + string.punctuation + r"]", sentence))
+
+        sentence = sentence.translate(
+            str.maketrans('', '', string.punctuation))
+
+        return sentence.strip()
+
     def predict_sentence(self, sentence: str) -> tuple:
         """
         Uses SVM and TF-IDF to predict the probability of a sentence.
@@ -30,7 +50,9 @@ class Classifier():
             A tuple (higher probability, label of the max probability)
         """
 
+        sentence = self.__pre_process(sentence)
         sentence = self.vectorizer.transform([sentence]).todense()
+
         pred = self.model.predict_proba(sentence)[0]
 
         return max(pred), self.labels[np.argmax(pred)]
